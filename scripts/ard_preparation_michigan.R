877a759549ebcdd8a91825abe74c4a9017bb87e4
ronnyhdez <ronny.hernandezm@gmail.com>
2024-03-24 15:54:45 -0600

    Ref #234 add michigan steps to create ARD

--- scripts/ard_preparation_michigan.R
+++ scripts/ard_preparation_michigan.R
@@ -29,21 +29,9 @@ source("R/summarized_indices.R")
 #   -   one_flux_weekly
 #   -   one_flux_monthly
 
-
 # Satellite data ----
 reflectance_500 <- 
   readRDS("data_satellite_processed/michigan_modis_reflectance_500_clean.rds")
-
-# Flux data ----
-## AmeriFluxBASE dataset to approximate fAPAR with sw_in and sw_out
-file <- "data/FLX_US-UMB_FLUXNET2015_FULLSET_2007-2017_beta-4/AMF_US-UMB_BASE_HH_18-5.csv"
-base_flux_hh <- read_csv(file, skip = 2) %>% 
-  clean_names() %>% 
-  # All NA values are written as -9999
-  mutate_all(~na_if(., -9999)) %>% 
-  mutate(date = ymd_hm(timestamp_start)) %>% 
-  select(-timestamp_start, -timestamp_end) %>% 
-  filter(year(date) >= 2015)
 
 ## Daily fluxes ----
 file <-
@@ -128,20 +116,7 @@ reflectance_500 <- filter_modis_pixels_500(data = reflectance_500,
 prepared_one_flux_daily <- one_flux_daily %>% 
   # mutate(fapar = ppfd_in - ppfd_out) %>%
   select(date, gpp_dt_vut_ref, gpp_nt_vut_ref) %>% 
-  filter(gpp_dt_vut_ref > params$gpp)
-
-# Prepare base dataset to obtain fapar
-prepared_base_flux_daily <- base_flux_hh %>% 
-  select(date, sw_in, sw_out) %>% 
-  mutate(date = ymd(as.Date(date))) %>% 
-  group_by(date) %>% 
-  summarise(
-    sw_in_mean = mean(sw_in, na.rm = TRUE),
-    sw_out_mean = mean(sw_out, na.rm = TRUE)
-  ) %>% 
-  ungroup() %>% 
-  mutate(fapar = 1 - (sw_out_mean / sw_in_mean)) %>% 
-  select(date, fapar)
+  filter(gpp_dt_vut_ref > 1)
 
 # Prepare reflectance datasets
 
@@ -155,13 +130,10 @@ daily_reflectance_500 <- summarise_indices(reflectance_dataset = reflectance_500
 
 ## 500m join
 daily_500 <- prepared_one_flux_daily %>% 
-  inner_join(daily_reflectance_500, by =  "date") %>% 
-  # Include the new base dataset with fapar calculated.
-  left_join(prepared_base_flux_daily, by = "date")
+  inner_join(daily_reflectance_500, by =  "date")
 
 # Remove objects
 rm(prepared_one_flux_daily, 
-   prepared_base_flux_daily,
    daily_reflectance_500)
 
 ### Weekly ----
@@ -171,7 +143,7 @@ rm(prepared_one_flux_daily,
 ## Check weeks in one flux weekly
 prepared_one_flux_weekly <- one_flux_weekly %>% 
   # mutate(fapar = ppfd_in - ppfd_out) %>% 
-  filter(gpp_dt_vut_ref > params$gpp) %>% 
+  filter(gpp_dt_vut_ref > 1) %>% 
   mutate(week_start = week(date_start),
          week_end = week(date_end),
          year = year(date_start)) %>% 
@@ -183,21 +155,6 @@ test <- prepared_one_flux_weekly %>%
 
 stopifnot(nrow(test) == 0)
 rm(test)
-
-# Prepare base dataset to obtain fapar
-prepared_base_flux_weekly <- base_flux_hh %>% 
-  select(date, sw_in, sw_out) %>% 
-  mutate(date = ymd(as.Date(date)),
-         week_start = week(date),
-         year = year(date)) %>% 
-  group_by(week_start, year) %>%
-  summarise(
-    sw_in_mean = mean(sw_in, na.rm = TRUE),
-    sw_out_mean = mean(sw_out, na.rm = TRUE)
-  ) %>% 
-  ungroup() %>% 
-  mutate(fapar = 1 - (sw_out_mean / sw_in_mean)) %>% 
-  select(week_start, year, fapar)
 
 # Prepare reflectance datasets
 
@@ -211,34 +168,18 @@ weekly_reflectance_500 <- summarise_indices(reflectance_dataset = reflectance_50
 
 ## 500m join
 weekly_500 <- prepared_one_flux_weekly %>% 
-  inner_join(weekly_reflectance_500, by = c("week_start" = "week", "year")) %>% 
-  left_join(prepared_base_flux_weekly, by = (c("week_start", "year")))
+  inner_join(weekly_reflectance_500, by = c("week_start" = "week", "year")) 
 
 # Remove objects
 rm(weekly_reflectance_500,
-   prepared_one_flux_weekly,
-   prepared_base_flux_weekly)
+   prepared_one_flux_weekly)
 
 ### Monthly ----
 
 # Prepare oneflux daily dataset
 prepared_one_flux_monthly <- one_flux_monthly %>% 
   # mutate(fapar = ppfd_in - ppfd_out) %>% 
-  filter(gpp_dt_vut_ref > params$gpp) 
-
-# Prepare base dataset to obtain fapar
-prepared_base_flux_monthly <- base_flux_hh %>% 
-  select(date, sw_in, sw_out) %>% 
-  mutate(date = ymd(as.Date(date)),
-         date = zoo::as.yearmon(date)) %>% 
-  group_by(date) %>% 
-  summarise(
-    sw_in_mean = mean(sw_in, na.rm = TRUE),
-    sw_out_mean = mean(sw_out, na.rm = TRUE)
-  ) %>% 
-  ungroup() %>% 
-  mutate(fapar = 1 - (sw_out_mean / sw_in_mean)) %>% 
-  select(date, fapar)
+  filter(gpp_dt_vut_ref > 1) 
 
 # Prepare reflectance datasets
 
@@ -252,12 +193,11 @@ monthly_reflectance_500 <- summarise_indices(reflectance_dataset = reflectance_5
 
 ## 500m join
 monthly_500 <- prepared_one_flux_monthly %>% 
-  inner_join(monthly_reflectance_500, by = "date") %>% 
-  left_join(prepared_base_flux_monthly, by = "date")
+  inner_join(monthly_reflectance_500, by = "date")
 
 # Remove objects
-rm(prepared_one_flux_monthly,
-   monthly_reflectance_500)
+rm(monthly_reflectance_500,
+   prepared_one_flux_monthly)
 
 ## Export analysis ready datasets ----
 
